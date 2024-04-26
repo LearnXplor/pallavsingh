2e9e0800b53cedcf6cfca73ae1df06711d3ca514
pallav.singh <pallav.singh@gingerwebs.co.in>
2024-04-27 01:33:14 +0530

    Pallav Singh Portfolio

---
+++ src/app/data.js
@@ -0,0 +1,100 @@
+
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Nextias.com",
+    description: "NEXT IAS is a coaching institute for UPSC CSE",
+    date: "2023-08-15",
+    demoLink: "https://www.nextias.com/",
+  },
+  {
+    id: 2,
+    name: "MadeEasy Prime",
+    description: "Now strengthen your exam preparation with MADE EASY",
+    date: "2024-03-20",
+    demoLink: "https://www.madeeasyprime.com/",
+  },
+  {
+    id: 3,
+    name: "MENIIT",
+    description: "MENIIT, an initiative of NEXT IAS & MADE EASY Group",
+    date: "2024-01-10",
+    demoLink: "https://budgetplanner.example.com",
+  },
+  {
+    id: 4,
+    name: "Recurpost",
+    description: "At RecurPost, we've made feedback handling",
+    date: "2023-05-30",
+    demoLink: "https://healthbeat.example.com",
+  },
+  {
+    id: 5,
+    name: "Millicom",
+    description: "leading provider of fixed and mobile telecom",
+    date: "2022-07-12",
+    demoLink: "https://www.millicom.com/",
+  },
+  {
+    id: 6,
+    name: "Telcovas",
+    description: "Telcovas' Free Roaming Solution, a groundbreaking service",
+    date: "2022-10-01",
+    demoLink: "https://www.telcovasworld.com/",
+  },
+  {
+    id: 7,
+    name: "Optum Health",
+    description: "Health Management Solution",
+    date: "2021-04-18",
+    demoLink: "https://www.optum.com/en/business/health-plans/members/care-management.html",
+  },
+  {
+    id: 8,
+    name: "Toppr",
+    description: "Lead as instructor for JS Courses",
+    date: "2022-11-05",
+    demoLink: "https://community.toppr.com/",
+  },
+  {
+    id: 9,
+    name: "BYJU's",
+    description: "Worked as an customer retention specialist",
+    date: "2021-03-22",
+    demoLink: "https://byjus.com/",
+  }
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Industrial Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Udemy",
+    link: "https://www.udemy.com/user/pallav-singh-12/",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/pallav-singh-angular/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Blogs",
+    link: "https://pallavsingh.hashnode.dev/",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ src/components/about/index.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            Architect of Frontend Development
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+            Hello, I'm Pallav Singh, and I'm a full time software developer and part time content creator. With 3+ years of experience in both fields, I have a unique perspective on how technology and content can work together to achieve business goals.
+
+            As a software developer, I specialize in end-to-end application development, and I enjoy tackling complex problems and coming up with elegant solutions. I'm proficient in Frontend Development and I'm always looking for ways to improve my skills and stay up-to-date with the latest technologies.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            8+ <sub className="font-semibold text-base">Industry Projects</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            3+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
+        >
+          {/* <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="CodeBucks"
+            loading="lazy"
+          /> */}
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
+          {/* <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="CodeBucks"
+            loading="lazy"
+          /> */}
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full"}>
+          <img
+            className="w-full h-auto"
+            src={`https://skillicons.dev/icons?i=angular,html,css,bootstrap,cloudflare,react,figma,firebase,git,github,js,jquery,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,redux,sass,supabase,tailwind,threejs,vercel,vscode`}
+            alt="Pallav Singh"
+            loading="lazy"
+          />
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          {/* <img
+            className="w-full h-auto"
+            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt="CodeBucks"
+            loading="lazy"
+          /> */}
+        </ItemLayout>
+
+        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
+          <Link
+            href="https://pallavsingh.hashnode.dev/"
+            target="_blank"
+            className="w-full"
+          >
+            {/* <img
+              className="w-full h-auto"
+              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
+              alt="CodeBucks"
+              loading="lazy"
+            /> */}
+          </Link>
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
